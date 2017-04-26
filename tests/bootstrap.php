816e8ac4802d7ce7c3326ae6a772004327e007fb
Gary Jones <gary@gamajo.com>
2017-04-26 14:25:00 +0100

    Tests: Conditionally load parts of bootstrap.php
    The condition is which testsuite has been requested to be run. This allows Unit tests to be run outside of a WordPress environment, while integration / system tests can run with WP loaded.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,22 +8,26 @@
  * @license      GPL-2.0+
  */
 
-$plugin_slug_tests_dir = getenv( 'WP_TESTS_DIR' );
+$argv = $GLOBALS['argv'];
+$key = array_search( '--testsuite', $argv );
 
-if ( ! $plugin_slug_tests_dir ) {
-	$plugin_slug_tests_dir = '/tmp/wordpress-tests-lib';
+if ( $key && 'Integration' === $argv[ $key + 1 ] ) {
+	$plugin_slug_tests_dir = getenv( 'WP_TESTS_DIR' );
+
+	if ( ! $plugin_slug_tests_dir ) {
+		$plugin_slug_tests_dir = '/tmp/wordpress-tests-lib';
+	}
+
+	// Give access to tests_add_filter() function.
+	require_once $plugin_slug_tests_dir . '/includes/functions.php';
+
+	/**
+	 * Manually load the plugin being tested.
+	 */
+	tests_add_filter( 'muplugins_loaded', function () {
+		require dirname( __DIR__ ) . '/plugin-slug.php';
+	} );
+
+	// Start up the WP testing environment.
+	require $plugin_slug_tests_dir . '/includes/bootstrap.php';
 }
-
-// Give access to tests_add_filter() function.
-require_once $plugin_slug_tests_dir . '/includes/functions.php';
-
-/**
- * Manually load the plugin being tested.
- */
-tests_add_filter( 'muplugins_loaded', function() {
-	require dirname( __DIR__ ) . '/plugin-slug.php';
-} );
-
-// Start up the WP testing environment.
-require $plugin_slug_tests_dir . '/includes/bootstrap.php';
-
