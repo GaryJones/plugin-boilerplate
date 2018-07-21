7faff39312eed95407ba36254894281e9e52863c
Justin Sternberg <me@jtsternberg.com>
2018-07-21 11:08:03 -0400

    Replace plugin name in admin notice

--- plugin-slug.php
+++ plugin-slug.php
@@ -55,7 +55,7 @@ if ( version_compare( PHP_VERSION, '7.1', '<' ) ) {
 	function plugin_slug_deactivation_notice() {
 		$notice = sprintf(
 			// Translators: 1: Required PHP version, 2: Current PHP version.
-			'<strong>Genesis JS / No JS</strong> requires PHP %1$s to run. This site uses %2$s, so the plugin has been <strong>deactivated</strong>.',
+			'<strong>Plugin Boilerplate</strong> requires PHP %1$s to run. This site uses %2$s, so the plugin has been <strong>deactivated</strong>.',
 			'7.1',
 			PHP_VERSION
 		);
