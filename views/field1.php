8b521f5eeba71eea3ca93a35a33c69429c9f116e
Gary Jones <github@garyjones.io>
2024-08-26 17:14:51 +0100

    Docs: Fix SPDX identifier
    This license identifier was changed some time ago to be more explicit.

--- views/field1.php
+++ views/field1.php
@@ -5,7 +5,7 @@
  * @package      Gamajo\PluginSlug
  * @author       Gary Jones
  * @copyright    2024 Gary Jones
- * @license      GPL-2.0+
+ * @license      GPL-2.0-or-later
  */
 
 declare( strict_types = 1 );
