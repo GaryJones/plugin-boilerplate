a76ca9b1f9381f44a26adb9b1ed89eb052634e9e
Gary Jones <github@garyjones.io>
2024-08-26 16:51:15 +0100

    Add Rector
    Starts with PHP 8.1 sets and a few extra backward-compatible rules.

---
+++ rector.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Php82\Rector\Encapsed\VariableInStringInterpolationFixerRector;
+use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
+use Rector\Php84\Rector\Param\ExplicitNullableParamTypeRector;
+
+return RectorConfig::configure()
+	->withPaths(
+		array(
+			__DIR__ . '/src',
+			__DIR__ . '/tests',
+			__DIR__ . '/views',
+			__DIR__ . '/init.php',
+			__DIR__ . '/plugin-slug.php',
+		)
+	)
+	->withPhpSets( php81: true )
+	// Changes from later PHP Sets that are backwards compatible:
+	->withRules(
+		array(
+			// 8.2
+			VariableInStringInterpolationFixerRector::class,
+
+			// 8.3
+			AddOverrideAttributeToOverriddenMethodsRector::class,
+
+			// 8.4
+			ExplicitNullableParamTypeRector::class,
+		)
+	)
+//	->withPreparedSets( deadCode: true, codeQuality: true, instanceOf: true, codingStyle: true )
+//	->withTypeCoverageLevel( 1 )
+	;
