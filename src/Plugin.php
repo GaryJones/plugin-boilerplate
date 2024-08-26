cce42e932fb4de15cd37bd3c3f8e6feb586d71a1
Gary Jones <github@garyjones.io>
2024-08-26 17:43:01 +0100

    Remove loading text domain
    WordPress core handles this all automatically, and more efficiently now.

--- src/Plugin.php
+++ src/Plugin.php
@@ -51,38 +51,4 @@ class Plugin {
 	public function __construct( ConfigInterface $config ) {
 		$this->processConfig( $config );
 	}
-
-	/**
-	 * Launch the initialization process.
-	 *
-	 * @since 0.1.0
-	 */
-	public function run(): void {
-		\add_action( 'plugins_loaded', $this->load_textdomain( ... ) );
-	}
-
-	/**
-	 * Load the plugin text domain.
-	 *
-	 * @since 0.1.0
-	 */
-	public function load_textdomain(): void {
-		/**
-		 * Plugin text domain.
-		 *
-		 * @var string
-		 */
-		$text_domain   = $this->config->getKey( 'Plugin.textdomain' );
-		$languages_dir = 'languages';
-		if ( $this->config->hasKey( 'Plugin/languages_dir' ) ) {
-			/**
-			 * Directory path.
-			 *
-			 * @var string
-			 */
-			$languages_dir = $this->config->getKey( 'Plugin.languages_dir' );
-		}
-
-		\load_plugin_textdomain( $text_domain, false, $text_domain . '/' . $languages_dir );
-	}
 }
