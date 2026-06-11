f526d900e5902b40d595bc02794242b32783566b
Gary Jones <github@garyjones.io>
2026-06-11 12:12:24 +0100

    chore: target PHP 8.4 set in Rector config
    Composer requires PHP ^8.4, so the PHP 8.2 set plus cherry-picked
    8.3/8.4 rules was stale: the php84 set already contains both of those
    rules. Match the project's declare() spacing style and extend the
    copyright range.

--- rector.php
+++ rector.php
@@ -4,15 +4,13 @@
  *
  * @package      Gamajo\PluginSlug
  * @author       Gary Jones
- * @copyright    2024 Gary Jones
+ * @copyright    2024-2026 Gary Jones
  * @license      GPL-2.0-or-later
  */
 
-declare(strict_types=1);
+declare( strict_types = 1 );
 
 use Rector\Config\RectorConfig;
-use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
-use Rector\Php84\Rector\Param\ExplicitNullableParamTypeRector;
 
 return RectorConfig::configure()
 	->withPaths(
@@ -23,15 +21,5 @@ return RectorConfig::configure()
 			__DIR__ . '/plugin-slug.php',
 		]
 	)
-	->withPhpSets( php82: true )
-	// Changes from later PHP Sets that are backwards compatible.
-	->withRules(
-		[
-			// 8.3.
-			AddOverrideAttributeToOverriddenMethodsRector::class,
-
-			// 8.4.
-			ExplicitNullableParamTypeRector::class,
-		]
-	)
+	->withPhpSets( php84: true )
 	->withPreparedSets( deadCode: true, codeQuality: true, instanceOf: true, codingStyle: true, typeDeclarations: true );
